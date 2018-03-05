930d22416adb130c61f18c25825f3ef2e24059b9
Alexander Rosenthal <alexrosenthalster@gmail.com>
2018-03-05 14:19:36 -0500

    feat/ hits weather app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
 const API_KEY  = '0e17abce572a27ddbd3f7a83211f76b6';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = fetch(url).then(response => response.json());
+
+  console.log('Request: ', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
 	constructor(props) {
 		super(props);
@@ -17,6 +20,8 @@ export default class SearchBar extends Component {
 	onFormSubmit(e) {
 		e.preventDefault();
 		// hit the backend weather API
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term: '' })
 	}
 	render() {
 		return (
@@ -34,3 +39,9 @@ export default class SearchBar extends Component {
 		)
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
